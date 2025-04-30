f0f6b06898cf772d3c2b8c6d16b045d05dd49ce1
raksha r <raksharamesh000@gmail.com>
2025-04-30 14:46:53 +0530

    Initial commit: Voice assistant using ElevenLabs

---
+++ voice_assistant.py
@@ -0,0 +1,71 @@
+import os
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs.conversational_ai.conversation import Conversation
+from elevenlabs.conversational_ai.default_audio_interface import DefaultAudioInterface
+from elevenlabs.types import ConversationConfig
+
+
+# Load the .env file
+load_dotenv()
+
+# Get your credentials
+AGENT_ID = os.getenv("fsNztl1jbc69aa1oVku2")
+API_KEY = os.getenv("sk_95841353d947443f450900f23fa05c9e9290c6b4472f4836")
+
+user_name = "Raksha"  
+schedule = "French claass at 10:00; Gym at 17:00"
+prompt = f"You are a helpful assistant. Your user has the following schedule: {schedule}."
+first_message = f"Hello {user_name}, how can I help you today?"
+
+conversation_override = {
+    "agent": {
+        "prompt": {
+            "prompt": prompt,
+        },
+        "first_message": first_message,
+    },
+}
+
+config = ConversationConfig(
+    conversation_config_override=conversation_override,
+    extra_body={},
+    dynamic_variables={},
+)
+
+client = ElevenLabs(api_key=API_KEY)
+
+conversation = Conversation(
+    client,
+    AGENT_ID,
+    config=config,
+    requires_auth=True,
+    audio_interface=DefaultAudioInterface(),
+    
+)
+
+# Callback when the agent gives a response
+def print_agent_response(response):
+    print(f"Agent: {response}")
+
+# Callback when the response is interrupted (if the user speaks while the assistant is talking)
+def print_interrupted_response(original, corrected):
+    print(f"Agent interrupted, truncated response: {corrected}")
+
+# Callback to print the user's voice transcription
+def print_user_transcript(transcript):
+    print(f"User: {transcript}")
+    
+    
+    conversation = Conversation(
+    client,
+    AGENT_ID,
+    config=config,
+    requires_auth=True,
+    audio_interface=DefaultAudioInterface(),
+    callback_agent_response=print_agent_response,
+    callback_agent_response_correction=print_interrupted_response,
+    callback_user_transcript=print_user_transcript,
+)
+
+conversation.start_session()
